3547be29e11ed7528b70aa382b57d5bc3ca26680
afroze9 <afrozeamjad@gmail.com>
2023-02-10 17:08:58 +0500

    Minor change to test docker pipeline

--- src/main/java/com/afroze/projectmanagement/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/com/afroze/projectmanagement/discoveryserver/DiscoveryServerApplication.java
@@ -16,7 +16,7 @@ public class DiscoveryServerApplication {
 	 * @param args command line arguments
 	 */
 	public static void main(String[] args) {
-		System.out.println("test1");
+		System.out.println("test2");
 		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 }
